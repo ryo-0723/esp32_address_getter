d222e70aa7656dd02d1763cc8602d37c27854a93
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2022-09-17 13:44:04 +0900

    Delete esp_address_getter.ino

--- esp_address_getter.ino
+++
@@ -1,9 +0,0 @@
-void setup(void) {
-  Serial.begin(115200);
-  uint8_t bt_mac[6];
-  esp_read_mac(bt_mac, ESP_MAC_BT);
-  Serial.printf("Bluetooth Mac Address => %02X:%02X:%02X:%02X:%02X:%02X\r\n", bt_mac[0], bt_mac[1], bt_mac[2], bt_mac[3], bt_mac[4], bt_mac[5]);
-}
-
-void loop() {
-}
